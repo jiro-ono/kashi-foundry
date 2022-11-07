9ac3ffddfe6fa323859905d6e8ba1700dae99675
0xJiro <jiro@sushi.com>
2022-11-08 00:25:48 +0100

    ported kashi to 0.8.15

---
+++ src/pairs/SushiStaticLendPair.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: UNLICENSED
+
+// Sushi Static Lending Pair
+
+pragma solidity ^0.8.15;
+
+
+
+/*contract SushiStaticLendPair {
+    // libraries
+
+    // events
+
+    // Immutables (for MasterContract and all clones)
+
+    // MasterContract variables
+
+    // Per clone variables
+    // Clone init settings
+
+
+
+
+
+    constructor() {
+
+    }
+
+    function init(bytes calldata data) public payable override {
+
+    }
+
+
+    function accrue() public {
+
+    }
+
+    /// ABRA FUNCTIONS
+    // ---------------
+    // function isCollaterizationSafe
+    // function _isSolvent
+    // modifier solvent
+    // function updateExchangeRate
+    // function _addTokens
+    // functions addCollateral
+    // function _removeCollateral
+    // function removeCollateral
+    // function _borrow
+    // function borrow
+    // function _repay
+    // function repay
+    // actions constants
+    // function _num
+    // function _bentoDeposit
+    // function _bentoWithdraw
+    // function _call
+    // struct CookStatus
+    // function cook
+    // function _cookActionLiquidate
+    // function liquidate
+    // function setSafeCollaterization
+    // function withdrawFees
+    // function setFeeTo
+    // function reduceSupply
+    // changeInterestRate
+    // changeBorrowLimit
+    // setBlacklistedCallee
+    // setAllowedSupplyReducer
+    // function changeBorrowLimit
+    // function repayForAll
+
+    /// KASHI-OLD FUNCTIONS
+    // ---------------------
+    // function isSolvent
+    // modifier solvent
+    // function updateExchangeRate
+    // function _addTokens
+    // function addCollateral
+    // function _removeCollateral
+    // function removeCollateral
+    // function _addAsset
+    // function addAsset
+    // function _removeAsset
+    // function removeAsset
+    // function _borrow
+    // function borrow
+    // function _repay
+    // function repay
+    // actions constants
+    // function _num
+    // function _bentoDeposit
+    // function _bentoWithdraw
+    // function _call
+    // struct CookStatus
+    // function cook
+    // function liquidate
+    // function withdrawFees
+    // function setSwapper
+    // function setFeeTo
+    // function 
+
+
+
+
+
+}*/
+
+
+
+
