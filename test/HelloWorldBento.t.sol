572b549d9670a0d73e14861305905cea11590bb6
0xJiro <jiro@sushi.com>
2022-11-15 03:57:49 +0100

    added additional examples to hello world

--- test/HelloWorldBento.t.sol
+++ test/HelloWorldBento.t.sol
@@ -4,18 +4,39 @@ pragma solidity ^0.8.15;
 import "solbase/tokens/ERC20/ERC20.sol";
 import "utils/BaseTest.sol";
 import "interfaces/IBentoBoxV1.sol";
+import "interfaces/IOracle.sol";
 
+import {console2} from "forge-std/console2.sol";
 
 contract HelloWorldBento is BaseTest {
     IBentoBoxV1 public bentoBox;
     ERC20 public sushi;
+    ERC20 public usdc;
+    IOracle chainLinkOracle;
 
     function setUp() public override {
-        forkMainnet(15831906);
+        forkMainnet(15971968);
         super.setUp();
-
+        
         bentoBox = IBentoBoxV1(constants.getAddress("mainnet.bentobox"));
+        chainLinkOracle = IOracle(constants.getAddress("mainnet.oracle.chainlinkV2"));
+
+        console2.log(address(chainLinkOracle));
+        
         sushi = ERC20(constants.getAddress("mainnet.sushi"));
+        usdc = ERC20(constants.getAddress("mainnet.usdc"));
+
+        bytes memory sushiUsdcOracleData = abi.encode(
+            constants.getAddress("mainnet.chainlink.usdc.eth"),
+            constants.getAddress("mainnet.chainlink.sushi.eth"),
+            1000000
+        );
+
+        console2.logBytes(sushiUsdcOracleData);
+
+        uint256 usdcSushiRate = chainLinkOracle.peekSpot(sushiUsdcOracleData);
+
+        console2.log(usdcSushiRate);
 
         address sushiWhale = 0xcBE6B83e77cdc011Cc18F6f0Df8444E5783ed982;
         vm.startPrank(sushiWhale);
---
+++ test/KashiPairTest.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "utils/BaseTest.sol";
+import "solbase/tokens/ERC20/ERC20.sol";
+import "interfaces/IBentoBoxV1.sol";
+
+contract KashiPairTest is BaseTest {
+    IBentoBoxV1 public bentoBox;
+    ERC20 public sushi;
+
+    /*function setUp() public override {
+        forkMainnet(15831906);
+        super.setUp();
+
+        bentoBox = IBentoBoxV1(constants.getAddress("mainnet.bentobox"));
+        
+
+    }*/
+
+
+}
